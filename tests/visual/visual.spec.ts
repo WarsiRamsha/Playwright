ccd4d71f26513b6146f24e703a30041be51b6eab
Ramsha Warsi <ramsha_warsi@hotmail.com>
2022-05-18 19:52:36 +0500

    Visual Regression Testing is done.

--- tests/visual/visual.spec.ts
+++ tests/visual/visual.spec.ts
@@ -1,7 +1,14 @@
 import { test, expect } from '@playwright/test'
 test.describe('Visual Regression Testing', () => {
   test('Screenshot of Homepage', async ({ page }) => {
-    await page.goto('Http:https://example.com/')
+    await page.goto('https://www.example.com/')
     expect(await page.screenshot()).toMatchSnapshot('Homepage.png')
+  })
+  test('Single Element Screenshot', async ({ page }) => {
+    await page.goto('https://www.example.com/')
+    const pageElementParagraph = await page.$('p')
+    expect(await pageElementParagraph.screenshot()).toMatchSnapshot(
+      'paragraph.png'
+    )
   })
 })
